e04348b792f3042c8694062704133758218c3e32
Stephen Mckellar <stephen@thingmaker.io>
2015-07-29 10:22:52 +0100

    Don't include the stats method in stats hash

--- lib/habit_client/user/stats.rb
+++ lib/habit_client/user/stats.rb
@@ -1,7 +1,6 @@
 require 'ostruct'
 require 'forwardable'
 require 'hashup'
-require 'habit_client/api_base'
 
 class HabitClient::User
 
@@ -17,8 +16,8 @@ class HabitClient::User
     def_delegator :stats, :maxHealth, :max_health
     def_delegator :stats, :maxMP, :max_mp
 
-    hashup :stats, :training, :buffs, :per, :int, :con, :str, :points,
-           :lvl, :gp, :exp, :mp, :hp, :player_class, :to_next_level,
+    hashup :training, :buffs, :per, :int, :con, :str, :points, :lvl,
+           :gp, :exp, :mp, :hp, :player_class, :to_next_level,
            :max_health, :max_mp
 
     def self.parse(stats)
