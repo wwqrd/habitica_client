0a48f09bf665bbcb38b94725b3bab4ad1491a1e4
Stephen Mckellar <stephen@thingmaker.io>
2016-09-09 00:21:49 +0100

    Fixed silly bug with undefined method -- probably need to make this easier to find in future

--- lib/habitica_client/task.rb
+++ lib/habitica_client/task.rb
@@ -23,7 +23,8 @@ class HabiticaClient
     attr_accessor :user_id, :reminders, :id, :text, :notes, :value, :priority,
                   :attribute, :type, :tags, :checklist, :value, :priority,
                   :challenge, :down, :up, :history, :streak, :frequency,
-                  :history, :completed, :every_x, :repeat, :collapse_checklist
+                  :history, :completed, :every_x, :repeat, :collapse_checklist,
+                  :group
 
     date_accessor :created_at, :updated_at, :start_date, :date
 
